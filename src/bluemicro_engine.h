7b283891f4915e8e52303e928e812a5029e58028
Pierre Constantineau <jpconstantineau@gmail.com>
2022-02-22 19:18:25 -0600

    initial template

---
+++ src/bluemicro_engine.h
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2022 BlueMicro_Engine_Arduino_Library contributors (https://github.com/jpconstantineau/BlueMicro_Engine_Arduino_Library/graphs/contributors)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+
+#endif
