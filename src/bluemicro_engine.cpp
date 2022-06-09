5deff4b8389972215180982903b3f0bc58230ee9
Pierre Constantineau <jpconstantineau@gmail.com>
2022-06-08 22:35:08 -0600

    Initial code

--- src/bluemicro_engine.cpp
+++ src/bluemicro_engine.cpp
@@ -3,3 +3,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include "bluemicro_engine.h"
+
+// command_less is used for sorting
+bool command_less (command_t i,command_t j) { return (i.first<j.first); }
