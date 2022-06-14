4af70a2272ffa46995a79b07d074a46fb3bff08e
Pierre Constantineau <jpconstantineau@gmail.com>
2022-06-13 22:41:09 -0600

    changes from toggle to write

--- examples/blinky/blinky.ino
+++ examples/blinky/blinky.ino
@@ -12,8 +12,11 @@ void setup() {
     // map of all possible commands
     // Put all the commands you want to define here
     SETUPCOMMAND(commandList, 0 , pinMode(LED_BUILTIN, OUTPUT));
-    SETUPCOMMAND(commandList, 10, digitalToggle(LED_BUILTIN));
-    SETUPCOMMAND(commandList, 20, digitalToggle(LED_BUILTIN));
+    SETUPCOMMAND(commandList, 10, digitalWrite(LED_BUILTIN, HIGH));
+    SETUPCOMMAND(commandList, 20, delay(250));
+    SETUPCOMMAND(commandList, 30, digitalWrite(LED_BUILTIN, LOW));
+    SETUPCOMMAND(commandList, 40, delay(250));
+
 
     // these are the commands that are run once - during the setup call
     // Put all the commands you want to run at the end of setup here
@@ -23,6 +26,8 @@ void setup() {
     // these are the command that are run for every scan - during the loop call
     // Put all the commands you want to run in the loop here
     ADDCOMMAND(loopQueue, 10 );
+    ADDCOMMAND(loopQueue, 20 );
+    ADDCOMMAND(loopQueue, 30 );
     
     // This command makes sure that all commands prepared for the setup are run
     run_setup_commands(); 
@@ -32,7 +37,7 @@ void loop() {
   // these are the commands that are additionnal to every scan
   // Put all the commands you want to run in the loop here
 
-  ADDCOMMAND(commandQueue, 20 );
+  ADDCOMMAND(commandQueue, 40 );
 
   // ideally, this is the only command you need to run all the commands you have setup previously.
   // interval is 1000 ms (1 second) 
