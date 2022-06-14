af2a9c67e99bebe3adc3879ca1f01845918b4068
Pierre Constantineau <jpconstantineau@gmail.com>
2022-06-13 22:33:46 -0600

    fix to std

---
+++ src/std_fix.h
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2022 BlueMicro_HID_Arduino_Library contributors (https://github.com/jpconstantineau/BlueMicro_HID_Arduino_Library/graphs/contributors)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef STD_FIX_H
+#define STD_FIX_H
+namespace std {
+  void __throw_length_error(char const*) { while(true); }
+
+  void __throw_bad_alloc() { while(true); }
+}
+#endif
